ca4ec04c7e696bc4e6bc6e1bc17a0b9099d754cd
Nelsoon-ds <davidgaming108@gmail.com>
2026-09-09 16:10:49 +0200

    sorted out the rendering of the different modules

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -17,27 +17,32 @@ const positionchangeview = document.getElementById('position-change-view');
 const playercomparison = document.getElementById('player-comparison')
 const views = ['#top100-blitz', '#player-comparison', '#position-change-view'];
 
-function render() {
 
-    window.addEventListener("hashchange", () => {
-        let location = location.hash
-        if (location in views) {
+
+
+
+
+
+function render(location) {
+        if (views.includes(location)) {
             switch (location) {
                 case "#top100-blitz":
                     top100blitz.classList.remove('hidden');
                     playercomparison.classList.add('hidden');
                     positionchangeview.classList.add('hidden');
+                    break;
                 case "#position-change-view":
                     positionchangeview.classList.remove('hidden');
                     top100blitz.classList.add('hidden');
                     playercomparison.classList.add('hidden');
+                    break;
                 case "#player-comparison":
                     playercomparison.classList.remove('hidden');
                     top100blitz.classList.add('hidden');
                     positionchangeview.classList.add('hidden');
+                    break;
             }
         }
-    })
 }
 
 
@@ -61,11 +66,9 @@ function renderTable(items) {
     const board = document.getElementById('top100-board');
     board.classList.remove('hidden');
     const tableBody = document.getElementById('table-body')
-    console.log(tableBody);
     tableBody.innerHTML = '';
     items.forEach(item => {
         const row = document.createElement('tr');
-        console.log(item.rating);
         row.innerHTML = `
         <td>${item.username}</td>
         <td>${item.rating}</td>
@@ -75,9 +78,10 @@ function renderTable(items) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    retrieveTop100();
-    showTop100();
+    window.addEventListener("hashchange", () => {
+        render(location.hash)
+    });
+    retrieveTop100().then(showTop100);
 })
 
-showTop100();
-render();
+
